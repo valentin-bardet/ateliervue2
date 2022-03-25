ef6f492e26a1b2766cc9db570f4156292dee18d6
valentin-bardet <72023225+valentin-bardet@users.noreply.github.com>
2022-03-25 10:09:59 +0100

    test

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.prototype.$apiWeb = axios.create({
 
+    baseURL: 'http://api.reunionou.app:19180/',
+    params: {},
+    headers: { "X-Requested-With": "XMLHttpRequest", }
+})
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
+    router,
+    store,
+    axios,
+    render: function(h) { return h(App) }
 }).$mount('#app')
---
+++ src/views/MyPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="mypage">
+    <h1>This is The Page</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Mypage",
+  components: {},
+  data() {
+    return {
+      user: null,
+    };
+  },
+  methods: {
+    load() {
+      this.axios
+        .get(
+          "http://api.reunionou.app:19180/myPage/?token=4c252d21a886af0c69ca6180f5dcb7994d297a39d70c8b9940879b3a45b3257a"
+        )
+        .then((response) => {
+          console.log(response.data);
+        });
+    },
+  },
+  created() {
+    this.load();
+  },
+};
+</script>
