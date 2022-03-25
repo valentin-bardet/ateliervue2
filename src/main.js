d166485333b1a267e3d4d802d7b6a19e88fed130
HugoGEORG <hugo.georg57@gmail.com>
2022-03-25 09:59:10 +0100

    mapHere

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
+    router,
+    store,
+    render: function(h) { return h(App) }
 }).$mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="app">
+    <mapVue  :center="center" />
   </div>
 </template>
+
+<script>
+import mapVue from '../components/mapVue'
+export default {
+  name: 'app',
+  components: {
+    mapVue
+    // Remove the HelloWorld.vue 
+  },
+  data() {
+return {
+    // we are this as prop to the mapVue component 
+  center:{ 
+    lat: 40.730610, 
+    lng: -73.935242
+    }
+}
+  
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
