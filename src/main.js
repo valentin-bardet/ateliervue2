56f2e17b9999cadf7c07d3b9ff3b5615185ca0f9
valentin-bardet <72023225+valentin-bardet@users.noreply.github.com>
2022-03-28 18:10:41 +0200

    api deployée

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 Vue.prototype.$apiWeb = axios.create({
-    baseURL: 'http://api.reunionou.local:19180/',
+    baseURL: 'http://149.91.80.75:19180/',
     params: {},
     withCredentials: true,
 })
 Vue.prototype.$apiAuth = axios.create({
-    baseURL: 'http://api.reunionou.auth:19480/',
+    baseURL: 'http://149.91.80.75:19480/',
     params: {},
 })
 
