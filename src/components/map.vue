6dd6020e9ede73b9ec872f144fb9989a20847fcc
valentin-bardet <72023225+valentin-bardet@users.noreply.github.com>
2022-03-28 08:53:27 +0200

    oui

---
+++ src/components/map.vue
@@ -0,0 +1,72 @@
+<template>
+  <l-map
+    :center="center"
+    :zoom="zoom"
+    class="map"
+    ref="map"
+    @update:zoom="zoomUpdated"
+    @update:center="centerUpdated"
+  >
+    <l-tile-layer :url="url">
+    </l-tile-layer>
+    <restaurant
+      v-for="marker in markers"
+      :key="marker.id"
+      :marker="marker"
+    >
+    </restaurant>
+  </l-map>
+</template>
+
+<script>
+import { LMap, LTileLayer } from "vue2-leaflet";
+import Restaurant from "./restaurant";
+import "leaflet/dist/leaflet.css";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    Restaurant,
+  },
+  props: {
+    lat: String,
+    long: String,
+  },
+  data() {
+    return {
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      center: [this.lat, this.long],
+      zoom: 12,
+      markers: [
+        {
+          id: 1,
+          imageUrl:
+            "https://www.pngfind.com/pngs/m/671-6710560_blue-map-marker-png-transparent-png.png",
+          coordinates: [this.lat, this.long],
+        },
+      ],
+    };
+  },
+  methods: {
+    zoomUpdated(zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated(center) {
+      this.center = center;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+}
+.map {
+  position: absolute;
+  width: 100%;
+  height: 200px;
+  overflow: hidden;
+}
+</style>
