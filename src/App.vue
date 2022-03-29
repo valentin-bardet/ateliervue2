155b0c46067b3d84fa9a4cb4fbcbab8a6e7edfec
valentin-bardet <72023225+valentin-bardet@users.noreply.github.com>
2022-03-29 13:18:22 +0200

    auto complete form

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,66 @@
 <template>
-<div id="app">
+  <div id="app">
     <b-navbar class="is-light">
-      <template #brand > 
-        <b-navbar-item >
-            <img
-                src="https://etapes.com/app/uploads/2016/05/1464094938.png"
-                alt="Coop VueJs"
-            >
+      <template #brand>
+        <b-navbar-item>
+          <img
+            src="https://etapes.com/app/uploads/2016/05/1464094938.png"
+            alt="Coop VueJs"
+          >
         </b-navbar-item>
       </template>
       <!--  -->
-      <template #start v-if="$store.state.token"> 
-        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+      <template
+        #start
+        v-if="$store.state.token"
+      >
+        <b-navbar-item
+          tag="router-link"
+          :to="{ path: '/' }"
+        >
           Home
         </b-navbar-item>
-        <b-navbar-item tag="router-link" :to="{ path: '/MesEventsCrees' }">
+        <b-navbar-item
+          tag="router-link"
+          :to="{ path: '/MesEventsCrees' }"
+        >
           My events
         </b-navbar-item>
-        <b-navbar-item tag="router-link" :to="{ path: '/CreateEvent' }">
+        <b-navbar-item
+          tag="router-link"
+          :to="{ path: '/CreateEvent' }"
+        >
           Create events
         </b-navbar-item>
       </template>
 
-      <template #end >
+      <template #end>
         <b-navbar-item tag="div">
-        <p v-if="$store.state.token">Connecté en tant que <strong>{{ $store.state.user.prenom }}</strong></p>
+          <p v-if="$store.state.token">Connecté en tant que <strong>{{ $store.state.user.prenom }}</strong></p>
         </b-navbar-item>
         <b-navbar-item tag="div">
-          <div class="buttons" v-if="!$store.state.token">
-            <router-link to="/Register" >
-            <b-button id="CreationCompte" type="is-success">Create an account</b-button>
-              
+          <div
+            class="buttons"
+            v-if="!$store.state.token"
+          >
+            <router-link to="/Register">
+              <b-button
+                id="CreationCompte"
+                type="is-success"
+              >Create an account</b-button>
+
             </router-link>
-            <router-link to="/Login" >
-                        <b-button type="is-info is-light">Login</b-button>
+            <router-link to="/Login">
+              <b-button type="is-info is-light">Login</b-button>
             </router-link>
           </div>
 
           <div v-else>
-            <b-button type="is-danger is-light" @click="deconnexion" class="button is-primary is-black">Sign out</b-button>
+            <b-button
+              type="is-danger is-light"
+              @click="deconnexion"
+              class="button is-primary is-black"
+            >Sign out</b-button>
           </div>
         </b-navbar-item>
       </template>
@@ -63,53 +85,25 @@
     
   </div> -->
 
-
-  
 </template>
 
 <script>
 export default {
-  mounted() {
-    this.$store.commit('setReady', false)
-
-    if (!this.$store.state.token) {
-      this.seConnecter()
-    } else {
-      this.$api.get(`users/${this.$store.state.user.id}/signedin`)
-      .then(this.demarrer)
-      .catch(this.seConnecter)
-    }
-  },
   methods: {
-    ready(){
-      this.$store.commit('setReady', true);
-    },
-    demarrer(){
-      this.$api.get('users').then(response => {
-        this.$store.commit('setusers', response.data)
-      })
-      this.ready();
-    },
-    seConnecter(){
-      this.$store.commit('setToken', false)
-      this.$router.push('/connexion')
-      this.ready();
-    },
-    deconnexion(){
+    deconnexion() {
       this.$store.state.token = null;
       this.$router.push("/login");
-    }
-  }
-}
-
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-#CreationCompte{
+#CreationCompte {
   margin-right: 30px;
 }
 
-.is-black{
+.is-black {
   margin-right: 10px;
 }
 #app {
